22722228e03003218c288c3e0b314a5db36580a6
Mariaberyl <Oyina001@gmail.com>
2022-09-20 08:56:52 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - main function in the task
@@ -10,5 +9,6 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (1);
+	return(0);
+
 }
